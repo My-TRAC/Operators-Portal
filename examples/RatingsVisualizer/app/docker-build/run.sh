a4473903e48d0f7165444092990b80f63479b483
joangui <joan@ac.upc.edu>
2019-03-14 16:12:52 +0100

    .

--- examples/RatingsVisualizer/app/docker-build/run.sh
+++ examples/RatingsVisualizer/app/docker-build/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-/root/scripts/waitForSchemaRegistry.sh
-/root/scripts/waitForKafkaConenct.sh
-/root/scripts/waitForMySQL.sh
-/root/scripts/setJDBCSinkConnect.sh
-#/root/scripts/infinite.sh
+git clone https://github.com/My-TRAC/ConfigurationScripts.git
+chmod +x ./ConfigurationScripts/*.sh
+
+
+/root/ConfigurationScripts/waitForSchemaRegistry.sh
+/root/ConfigurationScripts/waitForKafkaConenct.sh
+/root/ConfigurationScripts/waitForMySQL.sh
+/root/ConfigurationScripts/setJDBCSinkConnector.sh cigo-jdbc-sink_RatingsVisualizer
+#/root/ConfigurationScripts/infinite.sh
 env PORT=8080 /root/webapp-joan
