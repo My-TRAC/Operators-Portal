20a6e795227fb1fd829ba3e91dbd545394637526
joangui <joan@ac.upc.edu>
2019-07-09 14:46:05 +0200

    Adapted to new setJDBCSinkConnector.sh

--- examples/RatingsVisualizer/app/docker-build/run.sh
+++ examples/RatingsVisualizer/app/docker-build/run.sh
@@ -7,6 +7,17 @@ chmod +x ./ConfigurationScripts/*.sh
 /root/ConfigurationScripts/waitForSchemaRegistry.sh
 /root/ConfigurationScripts/waitForKafkaConenct.sh
 /root/ConfigurationScripts/waitForMySQL.sh
-/root/ConfigurationScripts/setJDBCSinkConnector.sh cigo-jdbc-sink_RatingsVisualizer
+setJDBCSinkConnector.sh 
+
+/root/ConfigurationScripts/setJDBCSinkConnector.sh  -c "cigo-jdbc-sink_RatingsVisualizer"\
+                                                    -k $KAFKA_CONNECT_HOST\
+                                                    -s $SCHEMA_REGISTRY_HOST_NAME\
+                                                    -n $SINK_TOPICS\
+                                                    -m $MYSQL_HOST\
+                                                    -d $MYSQL_DATABASE\
+                                                    -u $MYSQL_USER\
+                                                    -p $MYSQL_PASSWORD\
+                                                    -pk "mytrac_id"\
+                                                    -ac "true"
 #/root/ConfigurationScripts/infinite.sh
 env PORT=8080 /root/webapp-joan
