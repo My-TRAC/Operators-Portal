7533bbcfb3c0b9644ff83efe282f98f5d124cc59
joangui <joan@ac.upc.edu>
2019-10-02 14:36:35 +0200

    Sink connector add to retrive info from My-TRAC Platform

--- operators_portal/app/docker-build/run.sh
+++ operators_portal/app/docker-build/run.sh
@@ -11,6 +11,18 @@ chmod +x ./ConfigurationScripts/*.sh
 mysql -h$MYSQL_HOST -uroot -p$MYSQL_PASSWORD connect_test<create.sql
 
 
+
+/opt/RatingModeler/ConfigurationScripts/setJDBCSinkConnector.sh  -c "cigo-jdbc-sink_OperatorsPlatform"\
+                                                                 -k $KAFKA_CONNECT_HOST\
+                                                                 -s $SCHEMA_REGISTRY_HOST_NAME\
+                                                                 -n $SINK_TOPICS\
+                                                                 -m $MYSQL_HOST\
+                                                                 -d $MYSQL_DATABASE\
+                                                                 -u $MYSQL_USER\
+                                                                 -p $MYSQL_PASSWORD\
+                                                                 -pk "mytrac_id"\
+                                                                 -ac "true"
+
 /ConfigurationScripts/setJDBCConnector.sh/setJDBCSourceConnector.sh -c "cigo-jdbc-source_OperatorsPlatform"\
                                                 -k $KAFKA_CONNECT_HOST\
                                                 -m $MYSQL_HOST\
@@ -19,5 +31,7 @@ mysql -h$MYSQL_HOST -uroot -p$MYSQL_PASSWORD connect_test<create.sql
                                                 -p $MYSQL_PASSWORD\
                                                 -i "mytrac_id"\
                                                 -t "mytrac_last_modified"
+
+                                            
 
 sh /startup.sh
